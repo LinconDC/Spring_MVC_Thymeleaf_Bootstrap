58e6bff727166c8ecd2b0c75f9143bea741a4009
Lincon Martins <linconmartins18@gmail.com>
2022-08-05 09:06:34 -0300

    Repository and Spring Data JPA

--- demo/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ demo/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -9,18 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
-@Controller // Passo para o Spring que este é o controller
+@Controller													 // Passo para o Spring que este é o controller
 public class HomeController {
 	
-	@Autowired //Injeção de dependência 'Instância de PedidoRepository'
-	private PedidoRepository respository;
+	@Autowired												 //Injeção de dependência 'Instância de PedidoRepository'
+	private PedidoRepository pedidoRespository;
 	
-	@GetMapping("/home") // Anotação para mapear solicitações HTTP GET em métodos manipuladores especificos
-	public String home(Model model) { // Passando valores para VIEW com interface MODEL
-
-		List<Pedido> pedidos = respository.recuperaTodosOsPedidos();
-		model.addAttribute("pedidos", pedidos); // Mandando informaçoes para VIEW
-		return "home"; // Metodo String que retorna o nome da VIEW
+	@GetMapping("/home")									 // Anotação para mapear solicitações HTTP GET em métodos manipuladores especificos
+	public String home(Model model) {						 // Passando valores para VIEW com interface MODEL
+		List<Pedido> pedidos = pedidoRespository.findAll();	//Retorna uma lista com todos pedidos.
+		model.addAttribute("pedidos", pedidos);				// Mandando informaçoes para VIEW
+		return "home"; 										// Metodo String que retorna o nome da VIEW
 	}
 
 }
--- demo/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ demo/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -1,27 +1,11 @@
 package br.com.alura.mvc.mudi.repository;
 
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.alura.mvc.mudi.model.Pedido;
 
 //Passando para o Spring que essa classe é um repositório para criar instâncias quando for solicitado
-@Repository 
-public class PedidoRepository {
-
-	// A JPA comunica-se com o BD via EntityManager e o HIBERNATE configura com
-	// @PersistenceContext
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public List<Pedido> recuperaTodosOsPedidos() {
-
-		// Consulta
-		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
-		return query.getResultList();
-
-	}
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
 }
